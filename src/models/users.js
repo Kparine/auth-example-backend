df56750d34bef5be9072cdc4cb3049700120fffe
Kevin Parine <kparine314@gmail.com>
2018-11-26 11:03:15 -0800

    create

--- src/models/users.js
+++ src/models/users.js
@@ -1,14 +1,16 @@
 const db = require('../../db')
-const bcrypt = require('bcrypt-as-promised')
+const bcrypt = require('bcrypt')
 
 //////////////////////////////////////////////////////////////////////////////
 // Basic CRUD Methods
 //////////////////////////////////////////////////////////////////////////////
 
-function getOneByUserName(username){
+function getOneByUserName(username) {
   return (
     db('users')
-    .where({ username })
+    .where({
+      username
+    })
     .first()
   )
 }
@@ -24,9 +26,29 @@ function getOneByUserName(username){
 // 5. "return/continue" promise
 //////////////////////////////////////////////////////////////////////////////
 
-function create(username, password){
+function create(username, password) {
+  return getOneByUserName(username)
+    .then(function (data) {
+      if (data) throw {
+        status: 400,
+        message: 'User already exists'
+      }
 
-
+      return bcrypt.hash(password, 10)
+    })
+    .then(function (hashedPassword) {
+      return (
+        db('users')
+        .insert({
+          username,
+          password: hashedPassword
+        })
+        .returning('*')
+      )
+    })
+    .then(function ([data]) {
+      delete data.password
+    })
 }
 
 module.exports = {
